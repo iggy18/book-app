9aad8a3740a175afab56e6a28157919bbefddc4e
Mark Bell <skrambelled@gmail.com>
2020-10-21 21:08:19 -0700

    lab 13

--- data/books.sql
+++ data/books.sql
@@ -2,15 +2,15 @@ DROP TABLE IF EXISTS books;
 
 CREATE TABLE books (
   id SERIAL PRIMARY KEY,
-  author VARCHAR(205),
+  authors VARCHAR(205),
   title VARCHAR(250),
   description TEXT,
   cover TEXT,
   isbn VARCHAR(13)
 );
 
-INSERT INTO books (author, title, description, cover, isbn)
+INSERT INTO books (authors, title, description, cover, isbn)
   VALUES ('author', 'title', 'description', 'https://i.imgur.com/J5LVHEL.jpg', '1234567890123');
 
-INSERT INTO books (author, title, description, cover, isbn)
+INSERT INTO books (authors, title, description, cover, isbn)
   VALUES ('author2', 'title2', 'description2', 'https://i.imgur.com/J5LVHEL.jpg', '5555555555555');
---
+++ public/js/library-button.js
@@ -0,0 +1,4 @@
+document.getElementById('library-button').addEventListener('click', () => {
+    window.location.href = '/';
+    //app.get('/', getLibrary);
+});
